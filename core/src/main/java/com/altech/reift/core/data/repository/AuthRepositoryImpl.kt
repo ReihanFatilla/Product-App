26720eb7e4f8adf4ad605508103938e6bfde3f85
ReihanFatilla <reihanfatilla@gmail.com>
2023-09-05 22:40:57 +0700

    [feat]: finished login function(except with google)

--- core/src/main/java/com/altech/reift/core/data/repository/AuthRepositoryImpl.kt
+++ core/src/main/java/com/altech/reift/core/data/repository/AuthRepositoryImpl.kt
@@ -1,23 +1,43 @@
 package com.altech.reift.core.data.repository
 
 import com.altech.reift.core.data.remote.RemoteDataSource
+import com.altech.reift.core.domain.model.AuthResult
 import com.altech.reift.core.domain.repository.AuthRepository
 import com.altech.reift.core.utils.AuthType
-import com.google.firebase.auth.AuthResult
+import kotlinx.coroutines.tasks.await
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
 
 class AuthRepositoryImpl(val remoteDataSource: RemoteDataSource): AuthRepository {
 
     val auth = remoteDataSource.firebaseAuth()
-    override suspend fun login(type: AuthType): AuthResult{
-        when(type){
+    override suspend fun login(type: AuthType): AuthResult {
+        return when(type){
             is AuthType.Anonymous -> {
-                auth.signInAnonymously()
+                suspendCoroutine { continuation ->
+                    auth.signInAnonymously()
+                        .addOnSuccessListener {
+                            continuation.resume(AuthResult(true, "Success Signing In"))
+                        }
+                        .addOnFailureListener { exception ->
+                            continuation.resume(AuthResult(false, exception.message))
+                        }
+                }
             }
             is AuthType.Custom -> {
-                auth.signInWithEmailAndPassword(type.email, type.password)
+                suspendCoroutine { continuation ->
+                    auth.signInWithEmailAndPassword(type.email, type.password)
+                        .addOnSuccessListener {
+                            continuation.resume(AuthResult(true, "Success Signing In"))
+                        }
+                        .addOnFailureListener { exception ->
+                            continuation.resume(AuthResult(false, exception.message))
+                        }
+                }
             }
             is AuthType.Google -> {
-
+                AuthResult(success = false, message = null)
             }
         }
     }
